a819fdba3c2b4afbcf4f575c1a00047737cb0d50
r.almeida <rafael.almeida@rbmweb.com.br>
2023-05-08 13:16:41 -0300

    fix: updating titles

--- exercicio.php
+++ exercicio.php
@@ -18,7 +18,7 @@ include('sessaoCookie/sessao.php');
 
 <body class="exercicio">
     <header class="cabecalho">
-        <h1>Curso PHP</h1>
+        <h1>AEE Week - Web Development Course</h1>
         <h2>Visualização do Exercício</h2>
     </header>
     <nav class="navegacao">   
--- login.php
+++ login.php
@@ -54,7 +54,7 @@
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
     <link rel="stylesheet" href="recursos/css/estilo.css">
     <link rel="stylesheet" href="recursos/css/login.css">
-    <title>Curso PHP</title>
+    <title>AEE Week - Web Development Course</title>
 </head>
 
 <body class="login">
